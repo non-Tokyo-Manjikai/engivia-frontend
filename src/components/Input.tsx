a31d1241399bf9b40eec6117b6bda6300cd8cb47
katan4722 <uver7770422@gmail.com>
2021-10-22 00:03:56 +0900

    不要なコード削除、スタイル変更

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,27 +1,12 @@
 import { styled } from "@stitches/react";
-import { Dispatch, memo, SetStateAction, useCallback } from "react";
 
-type Props = {
-  children: string;
-  value: string;
-  setValue: Dispatch<SetStateAction<string>>;
-};
 export const Input = styled("input", {
   border: "solid 1px gainsboro",
   type: "text",
-  padding: "9px 16px",
+  padding: "0.5rem 1rem",
   borderRadius: "6px",
   width: "100%",
-  height: "100%",
   fontSize: "14px",
-});
-
-export const InputComponent = memo((props: Props) => {
-  const handleChange = useCallback(
-    (e: any) => {
-      props.setValue(() => e.target.value);
-    },
-    [props]
-  );
-  return <Input placeholder={props.children} value={props.value} onChange={handleChange} />;
+  outline: "none",
+  "&:Focus": { border: "solid 1px skyblue" },
 });
