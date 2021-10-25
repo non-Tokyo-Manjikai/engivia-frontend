2cda61ab6bf8a03727193c182be3219889c931f6
mitchan <hs0127sh@gmail.com>
2021-10-25 22:46:09 +0900

    ⚡️ divの変更

--- src/components/shared/OnAirLIst.tsx
+++ src/components/shared/OnAirLIst.tsx
@@ -57,6 +57,10 @@ const Lower = styled("div", {
   justifyContent: "space-between",
 });
 
+const Div = styled("div", {
+  display: "flex",
+});
+
 const Date = styled("div", {
   color: "#677075",
   fontSize: "1.3rem",
@@ -87,7 +91,7 @@ const OnAirList = () => {
           <Air color="on">放送中</Air>
         </Upper>
         <Lower>
-          <div className="flex">
+          <Div>
             <Icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path
                 fill-rule="evenodd"
@@ -96,13 +100,13 @@ const OnAirList = () => {
               />
             </Icon>
             <Date>2021年5月24日</Date>
-          </div>
-          <div className="flex">
+          </Div>
+          <Div>
             <Icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
             </Icon>
             <Number>エンジビア数10</Number>
-          </div>
+          </Div>
         </Lower>
       </Card>
       <Card>
