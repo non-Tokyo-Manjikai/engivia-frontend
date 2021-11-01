2f31254a781649d381db9838ef2241fa3aed9cc3
miyasan_0301 <miyasan.19990301@gmail.com>
2021-11-02 01:16:13 +0900

    🚚  変数名&コンポーネント名変更

--- src/components/HehButton.tsx
+++ src/components/HehButton.tsx
@@ -1,111 +1,111 @@
+import type { VFC } from "react";
+import { memo } from "react";
 import { styled } from "src/utils";
 
-export const HehButton = () => {
-  const HehBottom = styled("div", {
-    position: "absolute",
-    top: "50px",
-    left: "45px",
-    transform: "rotateX( -50deg ) ",
-    backgroundColor: "$blue10",
-    width: "270px",
-    height: "170px",
-    borderRadius: "100%",
-    zIndex: "3",
-  });
+export const HeeButtonKit: VFC = memo(() => {
+	return (
+		<HeeButtonWrap>
+			<HeeButton>
+				<Hee>へぇ</Hee>
+				<HeeShadow />
+			</HeeButton>
+			<HeeBottom />
+			<Foundation />
+			<FoundationShadow />
+			<FoundationBottom />
+		</HeeButtonWrap>
+	);
+});
 
-  const HehShadow = styled("div", {
-    position: "absolute",
-    top: "90px",
-    left: "45px",
-    backgroundColor: "$blue10",
-    width: "270px",
-    height: "45px",
-    zIndex: "3",
-  });
+const HeeBottom = styled("div", {
+	position: "absolute",
+	top: "50px",
+	left: "45px",
+	transform: "rotateX( -50deg ) ",
+	backgroundColor: "$blue10",
+	width: "270px",
+	height: "170px",
+	borderRadius: "100%",
+	zIndex: "3",
+});
 
-  const Heh = styled("button", {
-    position: "absolute",
-    top: "5px",
-    left: "45px",
-    transform: "rotateX( -50deg )",
-    backgroundColor: "$blue8",
-    width: "270px",
-    height: "170px",
-    borderRadius: "100%",
-    textAlign: "center",
-    paddingBottom: "15px",
-    fontSize: "4rem",
-    color: "white",
-    fontWeight: "bold",
-    zIndex: "6",
-  });
+const HeeShadow = styled("div", {
+	position: "absolute",
+	top: "90px",
+	left: "45px",
+	backgroundColor: "$blue10",
+	width: "270px",
+	height: "45px",
+	zIndex: "3",
+});
 
-  const HehButton = styled("div", {
-    "&:active": {
-      [`& ${HehShadow}`]: {
-        height: "15px",
-        top: "80px",
-        transform: "translateY(40px)",
-      },
-      [`& ${Heh}`]: {
-        transform: "translateY(30px) rotateX( -50deg )",
-      },
-    },
-  });
+const Hee = styled("button", {
+	position: "absolute",
+	top: "5px",
+	left: "45px",
+	transform: "rotateX( -50deg )",
+	backgroundColor: "$blue8",
+	width: "270px",
+	height: "170px",
+	borderRadius: "100%",
+	textAlign: "center",
+	paddingBottom: "15px",
+	fontSize: "4rem",
+	color: "white",
+	fontWeight: "bold",
+	zIndex: "6",
+});
 
-  const Foundation = styled("div", {
-    position: "absolute",
-    left: "15px",
-    top: "75px",
-    transform: "rotateX( -30deg )",
-    backgroundColor: "$slate5",
-    width: "330px",
-    height: "170px",
-    borderRadius: "100%",
-    zIndex: "2",
-  });
+const HeeButton = styled("div", {
+	"&:active": {
+		[`& ${HeeShadow}`]: {
+			height: "15px",
+			top: "80px",
+			transform: "translateY(40px)",
+		},
+		[`& ${Hee}`]: {
+			transform: "translateY(30px) rotateX( -50deg )",
+		},
+	},
+});
 
-  const FoundationBottom = styled("div", {
-    position: "absolute",
-    left: "15px",
-    top: "100px",
-    transform: "rotateX( -40deg )",
-    backgroundColor: "$slate8",
-    width: "330px",
-    height: "170px",
-    borderRadius: "100%",
-    zIndex: "1",
-  });
+const Foundation = styled("div", {
+	position: "absolute",
+	left: "15px",
+	top: "75px",
+	transform: "rotateX( -30deg )",
+	backgroundColor: "$slate5",
+	width: "330px",
+	height: "170px",
+	borderRadius: "100%",
+	zIndex: "2",
+});
 
-  const FoundationShadow = styled("div", {
-    position: "absolute",
-    left: "15px",
-    top: "160px",
-    transform: "rotateX( -30deg ) ",
-    backgroundColor: "$slate8",
-    width: "330px",
-    height: "25px",
-    zIndex: "1",
-  });
+const FoundationBottom = styled("div", {
+	position: "absolute",
+	left: "15px",
+	top: "100px",
+	transform: "rotateX( -40deg )",
+	backgroundColor: "$slate8",
+	width: "330px",
+	height: "170px",
+	borderRadius: "100%",
+	zIndex: "1",
+});
 
-  const HehButtonWrap = styled("div", {
-    position: "relative",
-    height: "280px",
-    width: "360px",
-  });
+const FoundationShadow = styled("div", {
+	position: "absolute",
+	left: "15px",
+	top: "160px",
+	transform: "rotateX( -30deg ) ",
+	backgroundColor: "$slate8",
+	width: "330px",
+	height: "25px",
+	zIndex: "1",
+});
 
-  return (
-    <>
-      <HehButtonWrap>
-        <HehButton>
-          <Heh>へぇ</Heh>
-          <HehShadow />
-        </HehButton>
-        <HehBottom />
-        <Foundation />
-        <FoundationShadow />
-        <FoundationBottom />
-      </HehButtonWrap>
-    </>
-  );
-};
+const HeeButtonWrap = styled("div", {
+	position: "relative",
+	height: "280px",
+	width: "360px",
+});
