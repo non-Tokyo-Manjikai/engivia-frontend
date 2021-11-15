e1741d6bcca3b86be7e8567eac24b603056457e1
miyasan_0301 <miyasan.19990301@gmail.com>
2021-11-15 23:53:58 +0900

    ♻️ 初期値修正

--- src/hooks/useGetEngiviaInfo.ts
+++ src/hooks/useGetEngiviaInfo.ts
@@ -6,23 +6,16 @@ import fetch from "unfetch";
 
 const fetchWithToken = (url: string, token: string) => {
 	return fetch(url, {
+		method: "GET",
 		headers: {
 			Authorization: `Bearer ${token}`,
 		},
 	}).then((res) => res.json());
 };
 
-const initialData: BroadcastLiveType = {
-	id: 0,
-	title: "",
-	status: "live",
-	scheduleStartTime: new Date(),
-	Trivia: [],
-};
-
 export const useGetEngiviaInfo = (url: string, token: string) => {
 	const { data, error } = useSWR<BroadcastLiveType>([`${API_URL}${url}`, token], fetchWithToken, {
-		fallbackData: initialData,
+		fallbackData: {} as BroadcastLiveType,
 	});
 
 	return {
@@ -32,15 +25,3 @@ export const useGetEngiviaInfo = (url: string, token: string) => {
 		isEmpty: data && data === undefined,
 	};
 };
-
-// // ----------------------------------------
-
-// export const useSharedState = <T extends any>(key: string, fallbackData?: T) => {
-// 	const { data, mutate } = useSWR(key, { fallbackData });
-// 	return [data, mutate] as [typeof fallbackData, KeyedMutator<typeof fallbackData>];
-// };
-
-// export const useCounter = (url: string) => {
-// 	const [count, setCount] = useSharedState(url, 0);
-// 	return { count, setCount };
-// };
