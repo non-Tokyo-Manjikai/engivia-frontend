34c7cf9238183558c8bd03864703584774b18d4f
aopontann <aopontan0416@gmail.com>
2021-11-17 23:59:35 +0900

    ユーザーステートにトークンを追加 Recoil #54

--- src/components/atoms.ts
+++ src/components/atoms.ts
@@ -8,5 +8,6 @@ export const userInfoState = atom({
     isAdmin: false,
     image:
       "https://secure.gravatar.com/avatar/e57b3678017c2e646e065d9803735508.jpg?s=512&d=https%3A%2F%2Fa.slack-edge.com%2Fdf10d%2Fimg%2Favatars%2Fava_0013-512.png",
+    token: "token1"
   },
 });
