36e478c99e0a12bee2e850a26401c7d026f37ba5
aopontann <aopontan0416@gmail.com>
2021-11-17 23:54:42 +0900

    ✨ userInfoState #54

---
+++ src/components/atoms.ts
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+export const userInfoState = atom({
+  key: "userInfoState",
+  default: {
+    id: "user1",
+    name: "テストユーザー1",
+    isAdmin: false,
+    image:
+      "https://secure.gravatar.com/avatar/e57b3678017c2e646e065d9803735508.jpg?s=512&d=https%3A%2F%2Fa.slack-edge.com%2Fdf10d%2Fimg%2Favatars%2Fava_0013-512.png",
+  },
+});
