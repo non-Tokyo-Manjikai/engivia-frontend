88f12b94c398121dc362dc16ab75e53353d5233d
aopontann <aopontan0416@gmail.com>
2021-11-25 16:11:12 +0900

    ✨ fetch-postBroadcast #45 放送を作成リスクエスト処理を実装。 リクエスト結果のステータスコードを返すようにした。 ステータスコードが400, 500番台の場合エラーを返すようにした。

---
+++ src/hooks/postBroadcast.ts
@@ -0,0 +1,16 @@
+import { API_URL } from "src/constants/API_URL";
+import fetch from "unfetch";
+
+export const postBroadcast = (url: string, body: any, token: string) => {
+  return fetch(`${API_URL}${url}`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ...body }),
+  }).then((res) => {
+    if (res.status >= 400) throw new Error(`${res.status}`);
+    return res.status;
+  });
+};
