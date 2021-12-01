2ab5b10752349bc80d15ec1023272d63d018279a
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-01 13:07:39 +0900

    🏷️  型定義変更

--- src/components/EngiviaCard.tsx
+++ src/components/EngiviaCard.tsx
@@ -2,7 +2,7 @@ import type { VFC } from "react";
 import { styled } from "src/utils";
 
 type Props = {
-  id: number | string;
+  id: number;
   name: string;
   image?: string;
   content: string;
