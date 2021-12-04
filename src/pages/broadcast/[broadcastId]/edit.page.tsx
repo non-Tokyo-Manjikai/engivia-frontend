cd3924c8ac6bbd07247f8009c3123592fdb1f349
katan4722 <uver7770422@gmail.com>
2021-12-05 01:29:46 +0900

    ✨ 放送編集機能

--- src/pages/broadcast/[broadcastId]/edit.page.tsx
+++ src/pages/broadcast/[broadcastId]/edit.page.tsx
@@ -1,17 +1,90 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { format, utcToZonedTime } from "date-fns-tz";
 import { Button, Input, PageRoot, Title } from "src/components/styled";
+import { useGetEngiviaInfo } from "src/hooks/useGetEngiviaInfo";
 import { styled } from "src/utils";
+import { useCallback, useEffect, useState } from "react";
+import { handlePutTrivia } from "src/hooks/handlePutTrivia";
+import toast, { Toaster } from "react-hot-toast";
+import { useRecoilValue } from "recoil";
+import { userInfoState } from "src/components/atoms";
 
 const BroadcastEditPage: NextPage = () => {
+  const router = useRouter();
+  const userInfo = useRecoilValue(userInfoState);
+
+  //broadcast/[id]/live/adminから遷移してくる時にrouter.query.broadcastIdを渡してくる
+  const { data, isError, isEmpty } = useGetEngiviaInfo(`/broadcast/${router.query.broadcastId}`, userInfo.token);
+  const [dateValue, setDateValue] = useState<string>("");
+  const [titleValue, setTitleValue] = useState("");
+  console.log(userInfo.token);
+
+  if (isError) {
+    toast.error("エラーが起きました");
+  }
+  if (isEmpty) {
+    toast.error("データがありません");
+  }
+
+  useEffect(() => {
+    if (data?.id) {
+      const date = new Date(data.scheduledStartTime);
+      const jaDate = utcToZonedTime(date, "Asia/tokyo");
+      const NewFormatDate = format(jaDate, "yyyy-MM-dd", { timeZone: "Asia/tokyo" });
+      setDateValue(NewFormatDate);
+      setTitleValue(data.title);
+    }
+  }, [data]);
+
+  const handleDateChange = useCallback((e: any) => {
+    setDateValue(e.target.value);
+  }, []);
+
+  const handleTitleChange = useCallback((e: any) => {
+    setTitleValue(e.target.value);
+  }, []);
+
+  const handleCancel = () => {
+    router.push(`/broadcast/${router.query.broadcastId}/live/admin`);
+  };
+
+  const handleSaveEngiviaInfo = async () => {
+    const NewDate = new Date(dateValue);
+    const NewScheduledStartTime = NewDate.toISOString();
+    const body = {
+      token: userInfo.token,
+      scheduledStartTime: NewScheduledStartTime,
+      title: titleValue,
+    };
+    const toastId = toast.loading("Sending...");
+
+    const statusCode = await handlePutTrivia(`/broadcast/${router.query.broadcastId}`, body, body.token);
+    if (statusCode >= 400) {
+      toast.error(`error: ${statusCode}`, { id: toastId });
+    } else {
+      toast.success("保存成功しました", { id: toastId });
+      // トーストを表示した2秒後にページ遷移する
+      setTimeout(() => router.push(`/broadcast/${router.query.broadcastId}/live/admin`), 2000);
+    }
+  };
+
   return (
     <PageRoot>
       <Title>放送を編集</Title>
-      <Input type="text" placeholder="タイトルを入力する" />
-      <Input type="date" placeholder="2021/09/03" />
+      <Input type="text" placeholder="タイトルを入力する" value={titleValue} onChange={handleTitleChange} />
+      <Input type="date" value={dateValue} onChange={handleDateChange} />
       <ButtonWrap>
-        <Button color="primary">保存する</Button>
-        <Button color="secondary">キャンセル</Button>
+        <Button color="primary" onClick={handleSaveEngiviaInfo}>
+          保存する
+        </Button>
+        <Button color="secondary" onClick={handleCancel}>
+          キャンセル
+        </Button>
       </ButtonWrap>
+      <div>
+        <Toaster />
+      </div>
     </PageRoot>
   );
 };
