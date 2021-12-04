21bc2e65fc1995d3842e5e179d84ec9672dadd05
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-05 01:28:24 +0900

    🏷️  ドラッグイベントの型付け

--- src/components/dnd/Multicontainers.tsx
+++ src/components/dnd/Multicontainers.tsx
@@ -1,8 +1,10 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import {
   closestCorners,
   DndContext,
+  DragEndEvent,
+  DragOverEvent,
   DragOverlay,
+  DragStartEvent,
   KeyboardSensor,
   PointerSensor,
   useSensor,
@@ -41,14 +43,17 @@ export const Multicontainers: VFC<Props> = memo((props) => {
     const featuredTriviaList = broadcast?.Trivia?.reduce((nonFeature: number[], current: TriviaType) => {
       return current.featured ? [...nonFeature, current.id] : nonFeature;
     }, []);
-
     if (rootTriviaList && featuredTriviaList) {
       setItems((prev) => {
-        return {
-          root: [...rootTriviaList],
-          container1: [],
-          container2: [...featuredTriviaList],
-        };
+        if (prev.root.length === 0 && prev.container1.length === 0 && prev.container2.length === 0) {
+          return {
+            root: [...rootTriviaList],
+            container1: [],
+            container2: [...featuredTriviaList],
+          };
+        } else {
+          return prev;
+        }
       });
     }
   }, [broadcast]);
@@ -69,30 +74,29 @@ export const Multicontainers: VFC<Props> = memo((props) => {
     });
   };
 
-  const handleDragStart = (event: any) => {
+  const handleDragStart = (event: DragStartEvent) => {
     const { active } = event;
-    const { id } = active;
-    setActiveId(id);
+    const activeId = Number(active.id);
+    setActiveId(activeId);
   };
 
-  const handleDragOver = (event: any) => {
-    const { active, over, draggingRect } = event;
-    const { id } = active;
-    const { id: overId } = over;
-
+  const handleDragOver = (event: DragOverEvent) => {
+    const { active, over } = event;
+    const activeId = Number(active.id);
+    const overId = over?.id;
+    console.info(overId);
     const activeEngivia = broadcast?.Trivia.filter((item) => {
-      return item.id === id;
+      return item.id === activeId;
     })[0];
-
     if (!activeEngivia) return;
-
-    const activeContainer = findContainer(id);
+    const activeContainer = findContainer(activeId);
     const overContainer = findContainer(overId);
+    console.info(activeContainer);
+    console.info(overContainer);
 
     if (!activeContainer || !overContainer || activeContainer === overContainer) {
       return;
     }
-
     if (
       `${overContainer}` === "root" &&
       ((`${activeContainer}` === "container1" && activeEngivia.featured === true) ||
@@ -100,14 +104,12 @@ export const Multicontainers: VFC<Props> = memo((props) => {
     ) {
       return;
     }
-
     if (
       `${overContainer}` === "container1" &&
       (items.container1.length === 1 || `${activeContainer}` === "container2")
     ) {
       return;
     }
-
     // if (`${overContainer}` === "container2" && activeEngivia.featured === false) {
     // 	return;
     // }
@@ -116,29 +118,22 @@ export const Multicontainers: VFC<Props> = memo((props) => {
       setItems((prev: any) => {
         const activeItems = prev[activeContainer];
         const overItems = prev[overContainer];
-
-        const activeIndex = activeItems.indexOf(id);
-        const overIndex = overItems.indexOf(overId);
-
+        const activeIndex = activeItems.indexOf(activeId);
+        const overIndex = overItems.indexOf(over.id);
         let newIndex;
-        if (overId in prev) {
+        if (over.id in prev) {
           newIndex = overItems.length + 1;
         } else {
           const isBelowLastItem =
-            over &&
-            overIndex === overItems.length - 1 &&
-            draggingRect?.offsetTop > over.rect.offsetTop + over.rect.height;
-
+            over && overIndex === overItems.length - 1 && over.rect.offsetTop > over.rect.offsetTop + over.rect.height;
           const modifier = isBelowLastItem ? 1 : 0;
-
           newIndex = overIndex >= 0 ? overIndex + modifier : overItems.length + 1;
         }
-
         return {
           ...prev,
           [activeContainer]: [
             ...prev[activeContainer].filter((item: any) => {
-              return item !== active.id;
+              return item !== activeId;
             }),
           ],
           [overContainer]: [
@@ -151,42 +146,37 @@ export const Multicontainers: VFC<Props> = memo((props) => {
     }
   };
 
-  const handleDragEnd = async (event: any) => {
-    const { id } = event.active;
-    const { id: overId } = event.over;
-    const activeContainer = findContainer(id);
+  const handleDragEnd = async (event: DragEndEvent) => {
+    const { active, over } = event;
+    const activeId = Number(active.id);
+    const overId = over?.id;
+    const activeContainer = findContainer(activeId);
     const overContainer = findContainer(overId);
-
     if (!activeContainer || !overContainer || activeContainer !== overContainer) {
       return;
     }
-
     /* ============= 仮実装 ============= */
     if (overContainer === "container2") {
       console.info("フィーチャー済み");
-      const resultTrivia = broadcast?.Trivia.filter((trivia) => {
-        return trivia.id === id;
-      })[0];
       const PutBody = {
-        content: resultTrivia?.content,
         hee: props.totalHeeCount,
         featured: true,
         token: "token3",
       };
-      const result = await handlePutTrivia(`/trivia/${id}`, PutBody);
+      const result = await handlePutTrivia(`/trivia/${activeId}`, PutBody);
       setBroadcast((prevState) => {
         if (prevState) {
-          const resultTrivia = prevState.Trivia.map((trivia) => (trivia.id === result.id ? result : trivia));
+          const resultTrivia = prevState.Trivia.map((trivia) => {
+            return trivia.id === result.id ? { ...trivia, ...result } : trivia;
+          });
           return { ...prevState, Trivia: resultTrivia };
         }
         return prevState;
       });
       props.onWaitTitleCall();
     }
-
-    const activeIndex = items[activeContainer].indexOf(event.active.id);
-    const overIndex = items[overContainer].indexOf(overId);
-
+    const activeIndex = items[activeContainer].indexOf(activeId);
+    const overIndex = items[overContainer].indexOf(Number(overId));
     if (activeIndex !== overIndex) {
       setItems((items: any) => {
         return {
