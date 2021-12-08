9e2483e2ed153e67637186e9c961ecc99a50099a
katan4722 <uver7770422@gmail.com>
2021-12-09 00:23:16 +0900

    ✨ エンジビア編集機能追加

--- src/pages/broadcast/[broadcastId]/my_engivia/index.page.tsx
+++ src/pages/broadcast/[broadcastId]/my_engivia/index.page.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { useRecoilValue } from "recoil";
 import { BroadcastHeader, EngiviaCard } from "src/components";
 import { userInfoState } from "src/components/atoms";
 import { MyEngiviaInput } from "src/components/MyEngiviaInput";
 import { Button, PageRoot, Textarea } from "src/components/styled";
+import { handlePutTrivia } from "src/hooks/handlePutTrivia";
 import { useGetEngiviaInfo } from "src/hooks/useGetEngiviaInfo";
+import { useGetTrivia } from "src/hooks/useGetTrivia";
 import { styled } from "src/utils";
 
 const MyEngiviaPage: NextPage = () => {
@@ -14,41 +17,81 @@ const MyEngiviaPage: NextPage = () => {
   const { broadcastId } = router.query;
   const userInfo = useRecoilValue(userInfoState);
   const { data, isError, isLoading } = useGetEngiviaInfo(`/broadcast/${broadcastId}`, userInfo.token);
+  const {
+    data: trivia,
+    isError: triviaError,
+    isLoading: triviaIsLoading,
+  } = useGetTrivia(`/trivia?broadcastId=${broadcastId}`, userInfo.token);
+
   const [edit, setEdit] = useState(false);
+  const [text, setText] = useState<string>("");
+  const [buttonDisabledState, setButtonDisabledState] = useState(false);
 
   const handleEditToggle = useCallback(() => {
     setEdit((edit) => !edit);
   }, []);
 
+  useEffect(() => {
+    if (trivia) {
+      setText(trivia.content);
+    }
+  }, [trivia]);
+
   if (!broadcastId) return null;
+
+  const handleTextChange = (e: any) => {
+    setText(e.target.value);
+  };
+
+  const handleEditEngivia = async () => {
+    if (!text || !/\S/g.test(text)) {
+      toast.error("エンジビアを入力してください");
+      return;
+    }
+    setButtonDisabledState(true);
+    const toastId = toast.loading("Sending...");
+    const body = {
+      content: text,
+      token: userInfo.token,
+    };
+    const statusCode = await handlePutTrivia(`/trivia/${trivia?.id}`, body, body.token);
+    if (statusCode >= 400) {
+      toast.error("保存できませんでした", { id: toastId });
+      setButtonDisabledState(false);
+    } else {
+      toast.success("保存成功しました", { id: toastId });
+      setTimeout(() => router.push("/broadcast"), 2000);
+    }
+  };
 
   return (
     <PageRoot>
-      <BroadcastHeader status="upcoming" title={data?.title} />
+      <BroadcastHeader status={data?.status} title={data?.title} />
 
-      {isLoading ? (
+      {isLoading || triviaIsLoading ? (
         <div>loading</div>
-      ) : isError ? (
+      ) : isError || triviaError ? (
         <div>error</div>
       ) : !data?.id ? (
         <div>no data</div>
-      ) : data.Trivia?.length !== 0 ? (
+      ) : trivia?.statusCode !== 400 ? (
         <>
           {edit ? (
             <>
-              <Textarea value={data.Trivia[0].content} />
+              <Textarea value={text} onChange={handleTextChange} />
               <ButtonWrap>
-                <Button color="primary" onClick={handleEditToggle}>
+                <Button color="primary" onClick={handleEditEngivia} disabled={buttonDisabledState}>
                   保存する
                 </Button>
                 <Button color="secondary" onClick={handleEditToggle}>
                   キャンセル
                 </Button>
               </ButtonWrap>
+              <Toaster />
             </>
           ) : (
             <>
-              <EngiviaCard id={data.id} content={data.Trivia[0].content} name={data.Trivia[0].User.name} />
+              <EngiviaCard id={trivia?.id} content={text} name={trivia?.User?.name} />
               <ButtonWrap>
                 <Button color="primary" onClick={handleEditToggle}>
                   編集する
