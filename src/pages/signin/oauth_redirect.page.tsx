6f8807720f3e599712a16d18c7ce1a2a59ee5074
aopontann <aopontan0416@gmail.com>
2021-12-10 13:22:27 +0900

    cookieにユーザー情報を保存するようにした。 #102

--- src/pages/signin/oauth_redirect.page.tsx
+++ src/pages/signin/oauth_redirect.page.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useSetRecoilState } from "recoil";
-import { userInfoState } from "src/components/atoms";
+import { useCookies } from "react-cookie";
 import { PageRoot } from "src/components/styled";
 import { API_URL } from "src/constants/API_URL";
 import { styled } from "src/utils";
@@ -27,13 +26,19 @@ const fetcher = async (url: string) => {
 
 const signinRedirectPage: NextPage = () => {
   const router = useRouter();
-  const setUserInfoState = useSetRecoilState(userInfoState);
+  // const setUserInfoState = useSetRecoilState(userInfoState);
+  const [cookies, setCookie] = useCookies(["userInfo"]);
   // Slack認証をしてユーザー情報を取得する
   const { data, error } = useSWR<User>(`${API_URL}/slack/token?code=${router.query.code}`, fetcher);
-  if (data && !error) {
-    setUserInfoState(data);
+  if (data && !error && !cookies.userInfo) {
+    // setUserInfoState(data);
+    setCookie("userInfo", data);
+  }
+  if (cookies.userInfo) {
     router.push("/broadcast");
   }
+  console.dir(cookies);
+  console.log(`redirect cookies.userInfo: ${cookies.userInfo}`);
   return <PageRoot>{error ? <H1>認証エラー</H1> : data ? null : <H1>認証中</H1>}</PageRoot>;
 };
 
