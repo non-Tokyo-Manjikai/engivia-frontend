410918f6306f891e4c2e81681355e9e8b60e66d2
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-11 17:34:35 +0900

    🐛 exit_user

--- src/pages/broadcast/[broadcastId]/live/admin.page.tsx
+++ src/pages/broadcast/[broadcastId]/live/admin.page.tsx
@@ -73,6 +73,11 @@ const LiveAdminPage: NextPage = () => {
         prev.map((user: any) => (user.id === data.heeUser.id ? { ...user, heeCount: data.heeUser.count } : user)),
       );
     });
+
+    socket.on("exit_user", (data) => {
+      // console.info("すべての接続ユーザー取得", data);
+      setConnectUserList(data);
+    });
   };
 
   // タイトルコール送信
